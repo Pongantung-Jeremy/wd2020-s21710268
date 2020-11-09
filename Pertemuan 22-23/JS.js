ac99a72b620254679ef062ecd3fd70d2c324bf47
Ponganntung, Jeremy Andrew <69891981+Pongantung-Jeremy@users.noreply.github.com>
2020-11-09 22:44:18 +0800

    Add files via upload

---
+++ Pertemuan 22-23/JS.js
@@ -0,0 +1,74 @@
+/*
+Variable
+let & const
+let umur = 25;
+console.log(umur);
+umur = 26;
+let nilai = 95.5;
+let sudahMenikah = true;
+let $nama = "John Doe";
+console.log (_umur);
+console.log ($nama);
+
+let alamat;
+console.log(tahunLahir);
+
+
+
+-operator
+aritmatika operator (+, -, *, /)
+ let angka1 = 10;
+ let angka2 = 20;
+ console.log(angka1 + angka2);
+
+
+
+ let umur=30;
+ let namaDepan="John";
+ let namaBelakang="Halo";
+ console.log("Nama Saya " + 
+ namaDepan + " Nama Belakang Saya " +
+ namaBelakang + " Umur Saya " + umur);
+
+
+
+-operator perbandingan
+ let angka1 = 10;
+ let angka2 = 9;
+ console.log(angka1 <= angka2);
+
+
+
+-ternary operator
+ let angka = 5;
+ let hasil = angka <= 0 ? "Negatif" : "Positif";
+ console.log(hasil);
+
+
+
+-operator logika
+ let kondisi1 = 1<2;
+ let kondisi2 = true;
+
+
+
+ console.log(!kondisi1);
+ let angka = prompt("Input angka : ");
+  console.log("Angka yg diinput : " + angka);
+*/
+
+
+
+/*
+Exercise #7
+    Buatlah program JavaScript untuk menghitung BMI (Body Mass Index)
+    Rumus = berat (kg) / (tinggi * tinggi (m))
+    Gunakan prompt untuk meminta input dari user.
+*/
+
+let weight = prompt("input your weight (In Kilograms) = ");
+let height = prompt("Input Your Height (In Meters) = ");
+
+console.log (bmi = weight / (height*height));
+console.log ("Yout Body Mass Index = " +bmi);
+
